090d056e53945886772c9c43a7592b9eba948742
BaoNguyen09 <nttbvlogs@gmail.com>
2025-05-05 10:16:03 -0700

    feat: create a script that will do queries on servers

---
+++ get_servers.py
@@ -0,0 +1,48 @@
+from utils import get_db_connection
+
+def get_all_servers(user: str) -> list[dict]:
+    db = get_db_connection()
+    cursor = db.cursor()
+    # return all instances in database
+    cursor.execute("SELECT * FROM servers WHERE owner = %s;", (user,))
+    rows = cursor.fetchall()
+    db.commit()
+    cursor.close()
+    db.close()
+
+    resp = []
+    for row in rows:
+        resp.append(
+            {
+            "id": row[0],
+            "owner": row[1],
+            "description": row[2],
+            "instance_id": row[3],
+            "public_ip": row[4],
+            "ready": row[5]
+            }
+        )
+    return resp
+
+def get_server(primary_key: int, user: str) -> dict:
+    db = get_db_connection()
+    cursor = db.cursor()
+    # return a single instance in db
+    cursor.execute("SELECT * FROM servers WHERE id = %s AND owner = %s;", (primary_key, user,))
+    row = cursor.fetchone()
+    db.commit()
+    cursor.close()
+    db.close()
+
+    if row == None:
+        return {}
+    
+    resp = {
+            "id": row[0],
+            "owner": row[1],
+            "description": row[2],
+            "instance_id": row[3],
+            "public_ip": row[4],
+            "ready": row[5]
+        }
+    return resp
